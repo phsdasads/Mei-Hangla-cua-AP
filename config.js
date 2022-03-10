75fb4bcf45bc9f2667e69660294b36ea636464bb
phsdasads <93643686+phsdasads@users.noreply.github.com>
2022-03-11 00:25:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
 const CONFIG = {
-    introTitle: 'Bébi Mai Hằng ơi!',
+    introTitle: 'Bébi Mei Hằng phải không ạ ?',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro:'hihi',
-    title:'Anh xin lỗi Bébi vì đã làm em giận ạ...',
-    desc:'Anh không muốn Bébi giận đâu, tha lỗi cho anh nhớ ',
-    btnYes:'Tha nhưng bị phạt',
-    btnNo:'Không tha nhưng được ôm em',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em thế nhờ <3',
-    btnReply:'Gửi cho anh <3',
-    reply:'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess:'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc:'Thứ 7 anh chạy sang ôm công chúa của anh nháaaa.',
-    btnAccept:'Okiiiii lun <3',
-  
+    btnIntro: 'Ank Fong đẹp zai của em đây',
+    title: 'Anh ngoan, anh làm Bébi dỗi là do anh hư...',
+    desc: 'Bébi tha lỗi cho anh lần này nhớ ? ',
+    btnYes: 'Tha nhưng bị phạt',
+    btnNo: 'Không tha nhưng được ôm em',
+    question:'Anh iu Bébi wa chời wa đấc lunnn, iu to hơn cả Trái Đất nàyyyy',
+    btnReply: 'Bébi có iu Ank Fong khôngg',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà. Yêu em nhiều nhiều ơi là nhiều',
+    messDesc: 'Thứ 7 anh qua ôm công chúa của anh một cái nháaa.',
+    btnAccept: 'Okiiiii lun <3',
+    
+}
