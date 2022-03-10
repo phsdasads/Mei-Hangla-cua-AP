b04a635f9fe583b2031b8cc83bba2308930d661c
phsdasads <93643686+phsdasads@users.noreply.github.com>
2022-03-11 00:07:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Bébi Mai Hằng ơi!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Anh xin lỗi Bébi vì đã làm em giận ạ...',
+    desc: 'Anh không muốn Bébi giận đâu, tha lỗi cho anh nhớ ',
+    btnYes: 'Tha nhưng bị phạt',
+    btnNo: 'Không tha nhưng được ôm em',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em thế nhờ <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Thứ 7 anh chạy sang ôm công chúa của anh nháaaa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+  
